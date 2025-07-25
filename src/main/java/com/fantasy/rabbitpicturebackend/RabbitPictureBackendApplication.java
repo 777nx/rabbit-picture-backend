872472e33f6bfb14e0f3fbdb0c9010e4dcbc49a2
Fantasy <1362724990@qq.com>
2025-07-25 21:47:50 +0800

    项目初始化

---
+++ src/main/java/com/fantasy/rabbitpicturebackend/RabbitPictureBackendApplication.java
@@ -0,0 +1,17 @@
+package com.fantasy.rabbitpicturebackend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@MapperScan("com.fantasy.rabbitpicturebackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class RabbitPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fantasy/rabbitpicturebackend/RabbitPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasy.rabbitpicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
